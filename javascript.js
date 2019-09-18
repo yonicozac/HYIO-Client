d1a6048003cbf28597a4f9ce5f4d6875618bbb87
Yoni Cozac <yoni.cozac@algosec.com>
2019-09-18 23:10:43 +0300

    Add an option to encrypt given text by pattern, and submit encrypted text for decryption on the GCP

--- javascript.js
+++ javascript.js
@@ -1,19 +1,52 @@
+// Send each transposition variant to the GCP to transpose and return a score for us
 function decryptText(inputText) {
 
-	// Iterate transpose variations
-	permutateArray([1,2,3,4,5,6]).forEach(function(perm) {
-		console.log("decrypting for permutation " + perm.toString());
-		// For each transpose send to GCP, get back text
-		// Send text to GCP to score it
-		// Place in result box, sorted by score
+	$(".text_results > div").remove();
 
+	// Iterate transpose variations
+	permutateArray([1,2,3,4]).forEach(function(perm) {
+		console.log("decrypting for permutation " + perm.toString());
+		
+		// For each transpose - send to GCP, get back text and score
+		$.post('https://us-central1-fiery-bit-253008.cloudfunctions.net/ScoreText', {
+			text: inputText,
+			permutation: perm.join('') 
+		}, 
+		function(data, status) {
+			// Place in result box
+			$(".text_results")
+				.append($('<div></div>')
+					.attr('score', data.score)
+					.append($('<span></span>')
+						.text(data.score)
+					)
+					.append($('<span></span>')
+						.text(data.text)
+					)
+				)
+		});
 	});
-	// $.get('https://us-east1-fiery-bit-253008.cloudfunctions.net/SayHelloWorld?word=door', function(data, status) {
-		// console.log(`${data}`);
-	// });
 }
 
-function transposeText(transposePattern)  {
+// Transpose the given text by the pattern
+function transposeText(inputText, transposePattern)  {
+	return inputText
+		// Split into chunks of 4 characters
+		.match(/.{1,4}/g)
+		// For every chunk apply transposition
+		.map(chunk => {
+			let transposedChunk = new Array(4);
+			chunk.split('').forEach(function(char, i) {
+				transposedChunk[transposePattern[i]] = char;
+			})
+			return transposedChunk.join('');
+		})
+		// Re-assemble the string
+		.join('');
+}
+
+function applyTransposationOnText() {
+	$('.text_to_decrypt').val(transposeText($('.text_to_decrypt').val(), $('.transPattern').val()));
 }
 
 // Copied permutation calculation from https://stackoverflow.com/a/43260158
